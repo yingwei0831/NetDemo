74209e9be78ecf32886739c65801bbe1be122424
birneysky <birneysky@163.com>
2016-10-09 14:03:29 +0800

    modify

---
+++ app/src/main/java/com/jhhy/cuiweitourism/net/biz/VisaActionBiz.java
@@ -0,0 +1,7 @@
+package com.jhhy.cuiweitourism.net.biz;
+
+/**
+ * Created by zhangguang on 16/10/9.
+ */
+public class VisaActionBiz {
+}
---
+++ app/src/main/java/com/jhhy/cuiweitourism/net/models/FetchModel/VisaHotCountry.java
@@ -0,0 +1,7 @@
+package com.jhhy.cuiweitourism.net.models.FetchModel;
+
+/**
+ * Created by zhangguang on 16/10/9.
+ */
+public class VisaHotCountry {
+}

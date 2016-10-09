fd179355eb734ec4ecb71e472029ef2d43be79dc
birneysky <birneysky@163.com>
2016-10-09 14:05:03 +0800

    modify

--- app/src/main/java/com/jhhy/cuiweitourism/net/biz/BasicActionBiz.java
+++ app/src/main/java/com/jhhy/cuiweitourism/net/biz/BasicActionBiz.java
@@ -3,6 +3,15 @@ package com.jhhy.cuiweitourism.net.biz;
 import android.content.Context;
 import android.os.Handler;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.jhhy.cuiweitourism.net.models.ResponseModel.FetchResponseModel;
+import com.jhhy.cuiweitourism.net.netcallback.FetchResponse;
+
+import java.util.ArrayList;
+import java.util.IllegalFormatCodePointException;
+
 /**
  * Created by birney1 on 16/9/28.
  */
@@ -16,6 +25,42 @@ public class BasicActionBiz {
         this.handler = handler;
     }
 
+    public BasicActionBiz() {
+    }
+
+    public JsonElement parseJsonBody(FetchResponseModel model){
+        JsonParser parser = new JsonParser();
+        if(model.body != null && !("null".equals(model.body)) && model.body.length() > 0){
+            JsonElement element = parser.parse(model.body);
+            return element;
+        }
+        else {
+            return null;
+        }
+
+
+    }
+
+    public  ArrayList<ArrayList<String>> parseJsonTotwoLevelArray(FetchResponseModel model){
+        JsonElement element = parseJsonBody(model);
+        if (element != null && element.isJsonArray()) {
+            ArrayList<ArrayList<String>> array = new  ArrayList<ArrayList<String>>();
+            array = new Gson().fromJson(element, array.getClass());
+            return array;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public <T> T getOBject(Class<T> c) throws InstantiationException,IllegalAccessException{
+        T t = c.newInstance();
+        return t;
+    }
+
+    public <T> ArrayList<T>  parseJsonToTemplateArray(FetchResponseModel model,Class<T> c){
+        return  new ArrayList<T>();
+    }
 
 //    public void getBizMap{
 //        Map<String, Object> headMap = new HashMap<>();
--- app/src/main/java/com/jhhy/cuiweitourism/net/models/FetchModel/VisaHotCountry.java
+++ app/src/main/java/com/jhhy/cuiweitourism/net/models/FetchModel/VisaHotCountry.java
@@ -1,7 +1,13 @@
 package com.jhhy.cuiweitourism.net.models.FetchModel;
 
+import org.json.JSONObject;
+
 /**
  * Created by zhangguang on 16/10/9.
  */
-public class VisaHotCountry {
+public class VisaHotCountry extends  CarRentFetchModel {
+
+    public JSONObject toBizJsonObject(){
+        return super.toBizJsonObject();
+    }
 }

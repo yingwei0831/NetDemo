50300785d6b3a9a9e45f95c44f68e0ed57fad1d7
birneysky <birneysky@163.com>
2016-10-24 10:38:49 +0800

    m

--- app/src/main/java/com/jhhy/cuiweitourism/net/models/ResponseModel/TrainStationInfo.java
+++ app/src/main/java/com/jhhy/cuiweitourism/net/models/ResponseModel/TrainStationInfo.java
@@ -11,14 +11,16 @@ public class TrainStationInfo {
         public String name; //车站名
         public String fullPY; //车站名全拼
         public String shortPY; //车站名简拼
+        public String headChar;//
         public boolean isHot; //是否热点
         public int type ; //是否是标题
 
-    public TrainStationInfo(String id, String name, String fullPY, String shortPY, boolean isHot, int type) {
+    public TrainStationInfo(String id, String name, String fullPY, String shortPY, String headChar, boolean isHot, int type) {
         this.id = id;
         this.name = name;
         this.fullPY = fullPY;
         this.shortPY = shortPY;
+        this.headChar = headChar;
         this.isHot = isHot;
         this.type = type;
     }
@@ -74,6 +76,14 @@ public class TrainStationInfo {
         this.type = type;
     }
 
+    public String getHeadChar() {
+        return headChar;
+    }
+
+    public void setHeadChar(String headChar) {
+        this.headChar = headChar;
+    }
+
     @Override
     public String toString() {
         return "TrainStationInfo{" +
@@ -81,6 +91,7 @@ public class TrainStationInfo {
                 ", name='" + name + '\'' +
                 ", fullPY='" + fullPY + '\'' +
                 ", shortPY='" + shortPY + '\'' +
+                ", headChar='" + headChar + '\'' +
                 ", isHot=" + isHot +
                 ", type=" + type +
                 '}';
